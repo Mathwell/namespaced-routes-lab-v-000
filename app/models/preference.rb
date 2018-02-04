307d4299267c09beae7e46b4a0855dfc423adeaa
Olena Ageyeva <eageeva@hotmail.com>
2018-02-04 17:21:30 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -3,4 +3,8 @@ class Preference < ActiveRecord::Base
   def allow_create_artists=(value)
     @allow_create_artists=value
   end
+
+  def allow_create_songs=(value)
+    @allow_create_songs=value
+  end
 end
