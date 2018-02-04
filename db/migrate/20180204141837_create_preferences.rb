4ddd08a7953131753429b2fe474d71fba955d226
Olena Ageyeva <eageeva@hotmail.com>
2018-02-04 17:40:05 +0000

    automatically backed up by learn

--- db/migrate/20180204141837_create_preferences.rb
+++ db/migrate/20180204141837_create_preferences.rb
@@ -3,8 +3,8 @@ class CreatePreferences < ActiveRecord::Migration
     create_table :preferences do |t|
       t.string :artist_sort_order
       t.string :song_sort_order
-      t.boolean :allow_create_songs
-      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs, null: false, default: true
+      t.boolean :allow_create_artists, null: false, default: true
       t.timestamps null: false
     end
   end
