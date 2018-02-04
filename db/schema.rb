85da1931f0c5fc1fce45ca9f259458cc82a37803
Olena Ageyeva <eageeva@hotmail.com>
2018-02-04 17:00:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204141837) do
+ActiveRecord::Schema.define(version: 20160126235547) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "preferences", force: :cascade do |t|
-    t.string   "song_sort_order"
-    t.string   "artist_sort_order"
-    t.boolean  "new_song_allowed?"
-    t.boolean  "new_artist_allowed?"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
   end
 
   create_table "songs", force: :cascade do |t|
